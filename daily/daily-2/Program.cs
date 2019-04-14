8408ea8f7dd569ed96c9d1a14939857aa64e1de6
Luis Felipe <luisfelipekai@gmail.com>
2019-04-14 19:16:14 -0300

    [daily] Solve daily 2.

---
+++ daily/daily-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+    Given an array of integers, return a new array such that each element at index i of the new array 
+    is the product of all the numbers in the original array except the one at i.
+ */
+namespace daily_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[5] { 1, 2, 3, 4, 5 };
+
+            var solution = Solve(input);
+
+            PrintArray(solution);
+        }
+
+        static int[] Solve(int[] array) {
+            var newArray = new int[array.Length];
+            var totalProduct = 1;
+
+            for (int i = 0; i < array.Length; i++) {
+                totalProduct *= array[i];
+            }
+
+            for (int i = 0; i < newArray.Length; i++) {
+                newArray[i] = totalProduct / array[i];
+            }
+
+            return newArray;
+        }
+
+        static void PrintArray(int[] array) {
+            Console.Write("[ ");
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.Write(" ]");
+        }
+    }
+}
