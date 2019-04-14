17d8c5a78f773c541963dff09ab5dfc55c1900c4
Luis Felipe <luisfelipekai@gmail.com>
2019-04-14 20:24:00 -0300

    [daily] Solve daily 1

---
+++ daily/daily-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*Given a list of numbers and a number k, return whether any two numbers from the list add up to k. */
+namespace daily_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 10, 15, 3, 7 };
+            var k = 17;
+
+            var solution = Solve(input, k);
+
+            Console.WriteLine(solution);
+        }
+
+        static bool Solve(int[] array, int k) {
+            var set = new HashSet<int>();
+            
+            for (int i = 0; i < array.Length; i++) {
+                if (set.Contains(array[i])) {
+                    return true;
+                }
+
+                set.Add(k - array[i]);
+            }
+
+            return false;
+        }
+    }
+}
