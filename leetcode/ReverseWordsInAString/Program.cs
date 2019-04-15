7bb0ac35c93deec489d7bdc070f0a5be8ef6c6f8
Luis Felipe <luisfelipekai@gmail.com>
2019-04-15 07:44:40 -0300

    [leetcode] Solve Reverse Words in a String III

---
+++ leetcode/ReverseWordsInAString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*Given a string, you need to reverse the order of characters in each word within a
+ sentence while still preserving whitespace and initial word order. */
+namespace ReverseWordsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "Let's take LeetCode contest";
+
+            var solution = Solve(input);
+
+            Console.WriteLine(solution);
+        }
+
+        static string Solve(string word) {
+            var words = word.Split();
+        
+            for (int i = 0; i < words.Length; i++) {
+                var charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                
+                words[i] = new string(charArray);
+            }
+            
+            return string.Join(" ", words);
+        }
+    }
+}
