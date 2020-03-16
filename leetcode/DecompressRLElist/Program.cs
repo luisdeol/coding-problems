d455f25284edfa2239d24b30ddd848999cbaa76e
luisfelipe <luisdeol@hotmail.com>
2020-03-16 19:56:20 -0300

    [leetcode] Add description to Decompress run-length encoded list.

--- leetcode/DecompressRLElist/Program.cs
+++ leetcode/DecompressRLElist/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/*
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+Return the decompressed list.
+*/
 namespace DecompressRLElist
 {
     class Program
